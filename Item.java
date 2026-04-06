fd4000bbcdb0d75d206c2885b1be506deb632313
Imesh Senavirathna <2023ict064@stu.vau.ac.lk>
2026-04-06 17:37:37 +0530

    Refactor getters and toString method in Item class

--- Item.java
+++ Item.java
@@ -4,6 +4,7 @@ public class Item {
     private int quantity;
     private double price;
 
+    // Constructor
     public Item(String itemId, String itemName, int quantity, double price) {
         this.itemId = itemId;
         this.itemName = itemName;
@@ -11,17 +12,33 @@ public class Item {
         this.price = price;
     }
 
-    public String getItemId() { return itemId; }
-    public String getItemName() { return itemName; }
-    public int getQuantity() { return quantity; }
-    public double getPrice() { return price; }
+    // Getters
+    public String getItemId() {
+        return itemId;
+    }
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Setter
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
-    @Override
+    // toString()
     public String toString() {
-        return itemId + " | " + itemName + " | Qty: " + quantity + " | $" + price;
+        return "ID: " + itemId +
+               ", Name: " + itemName +
+               ", Quantity: " + quantity +
+               ", Price: " + price;
     }
 }
